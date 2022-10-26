be657279f43702960d5d3be2900b46c63e63703b
Elijah Noble <elijahnobledev@gmail.com>
2022-10-26 13:10:15 -0700

    error fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_29_191020) do
+ActiveRecord::Schema.define(version: 2022_10_26_185247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "task"
+    t.text "notes"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
